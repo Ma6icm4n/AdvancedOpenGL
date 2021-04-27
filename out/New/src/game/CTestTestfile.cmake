8d34b49a842532d896bda0aba0bc099bfcd2f90c
Magicman <clement.ciranna@viacesi.fr>
2021-04-27 10:09:20 +0200

    bonjour readme

---
+++ out/New/src/game/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Asus/Desktop/opengl-training-master2/opengl-training-master/AdvancedOpenGL/src/game
+# Build directory: C:/Users/Asus/Desktop/opengl-training-master2/opengl-training-master/AdvancedOpenGL/out/New/src/game
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
